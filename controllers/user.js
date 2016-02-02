5c1b639ec23f58a36fb67681c9b0d743030911e1
振震 <865702942@qq.com>
2016-02-02 16:32:22 +0800

    add assert

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const User = require('../db').User;
+const util = require('../lib/util');
 
 exports.login = function *() {
   let data = this.request.body;
+
+  this.assert(data.username, 400, '用户名不能为空!');
+  this.assert(data.password, 400, '密码不能为空!');
+
   let user = yield User.auth(data.username, data.password);
 
   this.assert(user, 400, '用户名或密码错误!');
@@ -14,9 +19,15 @@ exports.login = function *() {
 
 exports.register = function *() {
   let data = this.request.body;
-  let isReg = yield User.findByName(data.name);
 
-  this.assert(isReg, 400, '用户名已注册!');
+  this.assert(data.username, 400, '用户名不能为空!');
+  this.assert(data.password, 400, '密码不能为空!');
+  this.assert(util.isName(data.username), 400, '用户名格式错误!');
+  this.assert(data.password.length < 6, 400, '密码不能为空!');
+  this.assert(util.isEmail(data.email, true), 400, '邮箱格式错误!');
+
+  let isReg = yield User.findByName(data.username);
+  this.assert(!isReg, 400, '用户名已注册!');
 
   let user = yield User.add(data);
   this.session.user = user;
